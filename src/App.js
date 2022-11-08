798babddc1d34083478f8b6c90f1f6531f186995
dhar1128082 <dhravya31099194@gmail.com>
2022-11-08 16:20:55 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import './App.css';
+import "./App.css";
+import Movie from "./higherOrder/tech";
+import MovieReview from "./PureComponent/techField";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Higher Order Functions</h1>
-      <ul>
-        <li>A higher-order component (HOC) is an advanced technique in React for reusing component logic. </li>
-        <li> HOCs are not part of the React API,They are a pattern that emerges from React’s compositional nature.</li>
-        <li>a higher-order component is a function that takes a component and returns a new component.</li>
-      </ul>
-      <h3>
-      Reason to use Higher-Order component:
-      </h3>
-      <ul>
-        <li>Easy to handle</li>
-        <li>Get rid of copying the same logic in every component</li>
-      </ul>
-      <h1>Pure Components</h1>
-      <ul>
-        <li>Pure Components in React are the components which do not re-renders when the value of state and props has been updated with the same values</li>
-        <li>PureComponent Is Primarily Used for Performance Optimization.</li>
-        <li>We can use React.memo for transforming a component into pure component</li>
-      </ul>
-    </div>
+   
+      <>
+        <h1 style={{ textAlign:"center"}}>Higher-Order-Component</h1>
+        <p style={{fontSize:"20px"}}>HOC is a function which takes a Wrapped component as input argument and returns a new Enhanced component 
+It should always be a pure function. 
+It should never modify the Wrapped Component.</p>
+        <Movie id={1} />
+      
+      <div >
+        <h1 style={{ textAlign:"center"}}>Pure Component </h1>
+        <p style={{fontSize:"20px"}}>ReactJS has provided us a Pure Component. If we extend a class with Pure Component, there is no need for shouldComponentUpdate() Lifecycle Method. ReactJS Pure Component Class compares current state and props with new props and states to decide whether the React component should re-render itself or  Not.
+In simple words, If the previous value of state or props and the new value of state or props is the same, the component will not re-render itself. Since Pure Components restricts the re-rendering when there is no use of re-rendering of the component. Pure Components are Class Components which extends React.PureComponent. </p>
+        <MovieReview />
+      </div>
+    </>
   );
 }
 
